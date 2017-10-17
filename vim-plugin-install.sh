3300d7dde5b99e20c025865a006ba04345ebe135
Abhi <suriabhinav1997@gmail.com>
2017-10-16 23:10:21 -0400

    vim scripts

---
+++ vim-plugin-install.sh
@@ -0,0 +1,14 @@
+cd ~/.vim/bundle
+git clone https://github.com/mileszs/ack.vim.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/Yggdroot/indentLine.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/vim-syntastic/syntastic.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/vim-airline/vim-airline-themes.git
+git clone https://github.com/christoomey/vim-tmux-navigator.git
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.py --all
